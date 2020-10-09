11ca86fcd5ce007f73bfd0433e9be29a9c3a5888
davinnovation <davinnovation@gmail.com>
2020-10-09 11:31:18 +0900

    sampler custom callback priority (#22)
    Give first priority for custom call back

--- torchsampler/imbalanced.py
+++ torchsampler/imbalanced.py
@@ -41,14 +41,14 @@ class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
         self.weights = torch.DoubleTensor(weights)
 
     def _get_label(self, dataset, idx):
-        if isinstance(dataset, torchvision.datasets.MNIST):
+        if self.callback_get_label:
+            return self.callback_get_label(dataset, idx)
+        elif isinstance(dataset, torchvision.datasets.MNIST):
             return dataset.train_labels[idx].item()
         elif isinstance(dataset, torchvision.datasets.ImageFolder):
             return dataset.imgs[idx][1]
         elif isinstance(dataset, torch.utils.data.Subset):
             return dataset.dataset.imgs[idx][1]
-        elif self.callback_get_label:
-            return self.callback_get_label(dataset, idx)
         else:
             raise NotImplementedError
                 
