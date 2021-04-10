001e00deefb743b82f45b1e195be1f2fd2276f28
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-10 19:11:44 +0200

    update Install (#24)
    * make pkg installable (#1)
    
    * refactor installing
    * update readme
    * add CI tesing
    * pass imports
    
    * hint typing
    
    * Apply suggestions from code review

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 """A setuptools based setup module."""
 
-# Always prefer setuptools over distutils
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines, and accepts an argument
-#  to specify the text encoding Python 3 only projects can skip this import
-from io import open
+import sys
 from os import path
 
 from setuptools import setup
 
 try:
-    import builtins
+    from torchsampler import about
 except ImportError:
-    import __builtin__ as builtins
+    # alternative https://stackoverflow.com/a/67692/4521646
+    sys.path.append("torchsampler")
+    import about
 
-builtins.__LIGHTNING_SETUP__ = True
 PATH_HERE = path.abspath(path.dirname(__file__))
-
-import torchsampler  # noqa: E402
 
 with open(path.join(PATH_HERE, 'requirements.txt'), encoding='utf-8') as fp:
     requirements = [rq.rstrip() for rq in fp.readlines() if not rq.startswith('#')]
@@ -26,15 +21,16 @@ with open(path.join(PATH_HERE, 'requirements.txt'), encoding='utf-8') as fp:
 # Fields marked as "Optional" may be commented out.
 setup(
     name='torchsampler',
-    version=torchsampler.__version__,
-    url=torchsampler.__homepage__,
-    author=torchsampler.__author__,
-    author_email=torchsampler.__author_email__,
-    license=torchsampler.__license__,
-    description=torchsampler.__doc__,
+    version=about.__version__,
+    url=about.__homepage__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    license=about.__license__,
+    description=about.__doc__,
     packages=['torchsampler'],
-    keywords='sampler',
+    keywords='sampler,pytorch,dataloader',
     install_requires=requirements,
+    python_requires='>=3.6',
     include_package_data=True,
     classifiers=[
         'Environment :: Console',
@@ -49,7 +45,6 @@ setup(
         'Operating System :: OS Independent',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
 )
