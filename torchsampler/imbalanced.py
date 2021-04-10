57ac710cf840623dc2afa6178bc537fb8aae9de3
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-10 19:11:44 +0200

    update Install (#24)
    * make pkg installable (#1)
    
    * refactor installing
    * update readme
    * add CI tesing
    * pass imports
    
    * hint typing
    
    * Apply suggestions from code review

--- torchsampler/imbalanced.py
+++ torchsampler/imbalanced.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 import torch.utils.data
 import torchvision
@@ -5,32 +7,28 @@ import torchvision
 
 class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
     """Samples elements randomly from a given list of indices for imbalanced dataset
+
     Arguments:
-        indices (list, optional): a list of indices
-        num_samples (int, optional): number of samples to draw
-        callback_get_label func: a callback-like function which takes two arguments - dataset and index
+        indices: a list of indices
+        num_samples: number of samples to draw
+        callback_get_label: a callback-like function which takes two arguments - dataset and index
     """
 
-    def __init__(self, dataset, indices=None, num_samples=None, callback_get_label=None):
+    def __init__(self, dataset, indices: list = None, num_samples: int = None, callback_get_label: Callable = None):
         # if indices is not provided, all elements in the dataset will be considered
-        self.indices = list(range(len(dataset))) \
-            if indices is None else indices
+        self.indices = list(range(len(dataset))) if indices is None else indices
 
         # define custom callback
         self.callback_get_label = callback_get_label
 
         # if num_samples is not provided, draw `len(indices)` samples in each iteration
-        self.num_samples = len(self.indices) \
-            if num_samples is None else num_samples
+        self.num_samples = len(self.indices) if num_samples is None else num_samples
 
         # distribution of classes in the dataset
         label_to_count = {}
         for idx in self.indices:
             label = self._get_label(dataset, idx)
-            if label in label_to_count:
-                label_to_count[label] += 1
-            else:
-                label_to_count[label] = 1
+            label_to_count[label] = label_to_count.get(label, 0) + 1
 
         # weight for each sample
         weights = [1.0 / label_to_count[self._get_label(dataset, idx)] for idx in self.indices]
