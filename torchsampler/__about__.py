df2af5556ae0afcf8bead42f22ffd0b412bcfe71
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-14 11:31:58 +0200

    refactor about (#25)

--- torchsampler/__about__.py
+++ torchsampler/__about__.py
@@ -5,3 +5,12 @@ __license__ = "MIT"
 __homepage__ = "https://github.com/Borda/imbalanced-dataset-sampler",
 __doc__ = "A (PyTorch) imbalanced dataset sampler for oversampling low frequent classes" \
           " and undersampling high frequent ones."
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__doc__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
--- torchsampler/__init__.py
+++ torchsampler/__init__.py
@@ -1,11 +1,4 @@
-from torchsampler.about import (  # noqa: F401
-    __author__,
-    __author_email__,
-    __doc__,
-    __homepage__,
-    __license__,
-    __version__,
-)
+from torchsampler.__about__ import *  # noqa: F401 F403
 from torchsampler.imbalanced import ImbalancedDatasetSampler
 
 __all__ = [
