f6482a657745159632574b1acc07f20652f83f6f
LeeTaeHoon97 <59239082+LeeTaeHoon97@users.noreply.github.com>
2021-06-26 17:29:50 +0900

    Update imbalanced.py
    in case of ImageFolder, can return all of elements

--- torchsampler/imbalanced.py
+++ torchsampler/imbalanced.py
@@ -43,7 +43,7 @@ class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
         elif isinstance(dataset, torchvision.datasets.MNIST):
             return dataset.train_labels.tolist()
         elif isinstance(dataset, torchvision.datasets.ImageFolder):
-            return dataset.imgs[:][1]
+            return [x[1] for x in trainset.imgs]
         elif isinstance(dataset, torchvision.datasets.DatasetFolder):
             return dataset.samples[:][1]
         elif isinstance(dataset, torch.utils.data.Subset):
